04d1cf7d03a6bf2ac06bb00842bcc3e16e2d7bc3
farshid-azady <mcpd.developer@gmail.com>
2025-11-02 18:05:58 -0800

    make players name and symbol for dynamic purpos

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -1,17 +1,24 @@
-
 function App() {
-  
-
   return (
     <>
-    <h1>React Tic-Tac-Toe</h1>
-    
-    
+      <main>
+        <div id="game-container">
+          <ol id="players">
+            <li>
+              <span className="player-name">player 1</span>
+              <span className="player-symbol">X</span>{" "}
+            </li>
+            <li>
+              <span className="player-name">player 2</span>{" "}
+              <span className="player-symbol">O</span>
+            </li>
+          </ol>
+          GAME BOARD
+        </div>
+        LOG
+      </main>
     </>
-    
-   
-    
-  )
+  );
 }
 
-export default App
+export default App;
